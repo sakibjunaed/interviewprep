449da526bef9868f5b84f1387f19cc96e6537925
Sakib Junaed <sakibjunaed@gmail.com>
2025-10-20 17:14:19 -0400

    new aws terraform infrastructure

--- main.tf
+++ main.tf
@@ -5,3 +5,61 @@ terraform {
         region = "us-east-1"
     }
 }
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet" "mySubnet" {
+    id = "subnet-0ce1515776d6743d5"
+}
+
+resource "aws_efs_file_system" "efs" {
+    creation_token = "myefs"
+    performance_mode = "generalPurpose"
+    throughput_mode = "bursting"
+}
+
+resource "aws_security_group" "efs_sg" {
+    name = "efs-security-group"
+    description = "security group for EFS mount target"
+    vpc_id = data.aws_vpc.default.id
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_efs_mount_target" "efs_mount" {
+    file_system_id = aws_efs_file_system.efs.id
+    subnet_id = data.aws_subnet.mySubnet.id
+    security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_instance" "user-1" {
+    ami = "ami-0341d95f75f311023"
+    instance_type = "t3.micro"
+    subnet_id = data.aws_subnet.mySubnet.id
+    security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_instance" "user-2" {
+    ami = "ami-0341d95f75f311023"
+    instance_type = "t3.micro"
+    subnet_id = data.aws_subnet.mySubnet.id
+    security_groups = [aws_security_group.efs_sg.id]
+}
